c74c4f7733009f7b62c95daa15cefe7fdb840b32
scoobyshi <scott.justason@gmail.com>
2017-04-04 22:21:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Simple Node JS client for [etcd]: https://github.com/coreos/etcd
+
+# Getting Started
+1. npm install
+1. npm start
+
---
+++ index.js
@@ -0,0 +1,27 @@
+const { Etcd } = require("node-etcd3");
+const etcd = new Etcd(["etcd:2379"]);
+
+var ip = require("ip");
+var os = require("os");
+
+// console.log("address: ", ip.address(), "host: ", os.hostname());
+
+var instance = {
+  host: os.hostname(),
+  port: 5001,
+  ip: ip.address(),
+  name: "livingroom"
+}
+
+var  servicekey = "/services/" + instance.host;
+
+// With default lease, keep alive 10s (thus if app killed, dies within 10s
+etcd.set(servicekey, JSON.stringify(instance), "client").then( () => {
+  console.log("Key Created.");
+
+  etcd.get(servicekey, "json").then( (value) => {
+    console.log("Service Key : ", value);
+  });
+});
+
+
